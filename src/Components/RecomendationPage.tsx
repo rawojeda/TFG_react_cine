f2e7546368eb4407b4ae03b5f11d89c51586225a
rawojeda <raw_ojeda99@hotmail.com>
2022-06-11 10:40:54 +0200

    terminadas las recomendaciones

--- src/Components/RecomendationPage.tsx
+++ src/Components/RecomendationPage.tsx
@@ -2,35 +2,52 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { get } from "../Utils/Api_get";
 import { Recommendget } from "../Utils/BD_request";
-import { FilmDescription } from "../Utils/interfaces";
+import { filmData, FilmDescription, Recomendation } from "../Utils/interfaces";
+import MoviesGrid from "../Components/MoviesGrid";
+import { FaSadCry, FaSadTear } from "react-icons/fa";
 
-interface recommendId{
-  RecomendId:string;
+interface recommendId {
+  RecomendId: string;
 }
-export function RecomendationPage(){
-    const RecommendId: recommendId = useParams();
-    const URL_GETRECOMMEND = "http://localhost/bd-back/getRecommend.php";
-    const [recommend,setRecommend] = useState();
-    
-    function listado_peliculas(peliculas: string){
-        const films_string:string[] = peliculas.replaceAll(" ", "").split(",");
-        var films: FilmDescription[] = [];
-        films_string.map((movie)=>{
-          get("https://api.themoviedb.org/3/movie/" + movie+"?language=es-ES").then(
-            (data) => {
-              films.push(data);
-            }
-          );
-        })
-        return films;
-      }
-      const getrecommend = async () => {
-        const respRecommends= await Recommendget(URL_GETRECOMMEND, {recomendationId: +RecommendId.RecomendId});
-        setRecommend(respRecommends.Recomendation);
-      };
-      useEffect(() => {
-        getrecommend();
-      },[])
+export function RecomendationPage() {
+  const RecommendId: recommendId = useParams();
+  const URL_GETRECOMMEND = "http://localhost/bd-back/getRecommend.php";
+  const [recommend, setRecommend] = useState<Recomendation>();
+  const [films, setFilms] = useState<FilmDescription[]>([]);
+  const [totalFilms, setTotalFilms] = useState<number>();
 
-    return <div>Aqui saldran las recomendaciones</div>
+  function listado_peliculas(peliculas: string) {
+    const films_string: string[] = peliculas.replaceAll(" ", "").split(",");
+    setTotalFilms(films_string.length);
+    films_string.map((movie) => {
+      get(
+        "https://api.themoviedb.org/3/movie/" + movie + "?language=es-ES"
+      ).then((data: FilmDescription) => {
+        setFilms((prev) => prev.concat(data));
+      });
+    });
+  }
+  const getrecommend = async () => {
+    const respRecommends = await Recommendget(URL_GETRECOMMEND, {
+      recomendationId: +RecommendId.RecomendId,
+    });
+    setRecommend(respRecommends.Recomendation);
+    listado_peliculas(respRecommends.Recomendation.FilmsId);
+  };
+  useEffect(() => {
+    getrecommend();
+  }, []);
+
+  return (
+    <>
+      <p className="title">{recommend?.Title}</p>
+      <div className="film_list">
+        {films.length === totalFilms? 
+          <MoviesGrid films={films}/> : <>
+          <div className="filmdetail_box">
+            <FaSadTear className="emote-visible"/><h3>Todavía no hay peliculas para esta sección.</h3> <FaSadCry className="emote-visible"/>
+          </div></>}
+      </div>
+    </>
+  );
 }
